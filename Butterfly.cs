d56191d164e45c4a506cf5026f337c121e7cc58d
Dzida <dominik.dzida@o2.pl>
2024-11-05 22:07:26 +0100

    Main class creation

---
+++ Butterfly.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BOA
+{
+    class Butterfly : IOptimizationAlgorithm
+    {
+        public string Name { get; set; }
+        public double[] XBest { get; set; }
+        public double FBest { get; set; }
+        public int NumberOfEvaluationFitnessFunction { get; set; }
+
+
+        public Butterfly()
+        {
+            Name = "Butterfly optimization algorithm";
+        }
+
+        public double Solve()
+        {
+            return 0.2;
+        }
+    }
+}
--- IOptimizationAlgorithm.cs
+++ IOptimizationAlgorithm.cs
@@ -7,28 +7,15 @@ using System.Threading.Tasks;
 namespace BOA
 {
     public interface IOptimizationAlgorithm
- {
- // Nazwa algorytmu
- string Name { get; set; }
+     {
+         string Name { get; set; }
 
- // Metoda rozpoczynaj ąca rozwi ą zywanie zagadnienia poszukiwania
- // minimum funkcji celu .
- // Na pocz ątku sprawdza , czy w lokalizacji jest plik ze stanem algorytmu
- // w odpowiednim formacie . Jeśli taki plik istnieje , wznawia obliczenia
- // dla tego stanu ,
-// W przeciwnym razie zaczyna obliczenia od pocz ą tku .
- // Zwraca warto ść funkcji celu dla znalezionego rozwi ą zania
- // ( najlepszego osobnika )
- // double Solve();
+         double Solve();
 
- // Właś ciow ść zwracaj ąca tablic ę z najlepszym osobnikiem
- double[] XBest { get; set; }
+         double[] XBest { get; set; }
 
-// Właś ciwo ść zwracaj ąca warto ść funkcji dopasowania
- // dla najlepszego osobnika
- double FBest { get; set; }
+         double FBest { get; set; }
 
- // Właś ciwo ść zwracaj ąca liczb ę wywo łań funkcji dopasowania
- int NumberOfEvaluationFitnessFunction { get; set; }
- }
+         int NumberOfEvaluationFitnessFunction { get; set; }
+    }
 }
