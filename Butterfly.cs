a72e47ce80578085dd643311684106c7391c1e30
Dzida <dominik.dzida@o2.pl>
2024-11-06 21:43:21 +0100

    Population creation

--- Butterfly.cs
+++ Butterfly.cs
@@ -14,14 +14,44 @@ namespace BOA
         public double FBest { get; set; }
         public int NumberOfEvaluationFitnessFunction { get; set; }
 
+        private int[,] range;
+        private double[,] population;
+        private int population_size;
+        private int dim; 
 
-        public Butterfly()
+        public Butterfly(Func<double[], double> myFuncion,int dim_in,int itreations, int[,] range_in,int population_size_in)
         {
             Name = "Butterfly optimization algorithm";
+            range = range_in;
+            population_size = population_size_in;
+            dim = dim_in;
+        }
+        private void  CreatePopulation()
+        {
+            Random rand = new Random();
+            population = new double[population_size,dim];
+            for (int i = 0; i < population_size; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    population[i,j] = rand.NextDouble() * (range[j,1] - range[j, 0]) + range[j, 0];
+                }
+            }
+        }
+
+        private void FindBest()
+        {
+
+        }
+        private double CalculateNextPosition()
+        {
+            return 0.0;
         }
 
         public double Solve()
         {
+            CreatePopulation();
+  
             return 0.2;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
+
 namespace BOA
 {
     class Program
     {
+
+        static double f1(double[] xi)
+        {
+            double ret = 0;
+            foreach (double x in xi)
+            {
+                ret += Math.Abs(x);
+            }
+            double ret1 = 1;
+            foreach (double x in xi)
+            {
+                ret1 *= Math.Abs(x);
+            }
+            ret += ret1;
+            return ret;
+        }
         static void Main(string[] args)
         {
+            int[,]  range = new  int[4,2];
+       
+            Butterfly but = new Butterfly(f1,4,100,range,20);
             Console.WriteLine("Hello World!");
         }
+       
+       
     }
 }
